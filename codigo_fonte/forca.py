6648678b15536a4cd8aeb283638043487b0a6bc5
Gabriel Camargo da Silva <159748725+Meiz0n@users.noreply.github.com>
2026-04-10 11:39:05 -0300

    Code comment update
    Just an update to the job description within the code

--- codigo_fonte/forca.py
+++ codigo_fonte/forca.py
@@ -14,17 +14,20 @@ while True:
     print(' '.join(oculta))
     cont = 0
 
+    # Esconder palavra do usuario
     while "_" in oculta:
         print()
         if letras:
             print(f'Digitadas: {letras}')
         
+        #Chegem se entrada é letra
         while True:
             busca = input("Digite uma letra para advinhar: ").upper().strip()
             if len(busca) == 1 and busca.isalpha():
                 break
             print('Erro, digite apenas UMA LETRA.')
         
+        #Verificar se letra ja foi digitada
         print("=" * 33)
         if busca in letras:
             print(f"A letra {busca} já foi digitada.")
@@ -33,12 +36,15 @@ while True:
         letras.append(busca)
         print()
 
+        #Buscar letra nova na palavra escolhida
         if busca in palavra:
             for c, i in enumerate(palavra):
                 if busca == i:
                     oculta[c] = busca
             print(' '.join(oculta))
             print("." * 30)
+
+        #Acresenta uma parte do boneco na forca caso não tenha letra na palavra escolhida
         else:
             cont += 1
             print(f'A palavra nao tem a letra {busca}! Tente de novo.')
@@ -75,11 +81,13 @@ while True:
         if "_" not in oculta:
             print("Você venceu!")
 
+    #Looping para jogar novamente
     while True:
         continuar = input('Quer jogar novamente? [S/N]').strip().upper()
         if continuar in 'SN':
             break
         print('Digite [S/N]')
     
+    #Fim do programa
     if continuar == 'N':
         break
